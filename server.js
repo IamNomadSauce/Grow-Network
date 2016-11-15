901a37a9a313443f490f88f66d1eb328cb1eb9de
mgarsteck <g-photography@hotmail.com>
2016-11-15 15:00:00 -0800

    Update middleware for localhost

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
+
+// TODO - fix the http to https for cannareports :)
 app.use(function (req, res, next) {
-  if(req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if(req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
